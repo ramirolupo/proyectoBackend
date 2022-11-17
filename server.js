1f63c84155cbf8544d0d892ce151a392e772fd5b
Ramiro Lupo <luporamiro5@gmail.com>
2022-11-17 18:32:52 -0300

    LOG-IN POR FORMULARIO

--- server.js
+++ server.js
@@ -2,29 +2,61 @@ const express = require('express');
 const { Server: HttpServer } = require('http');
 const { Server: IOServer } = require('socket.io');
 const { engine } = require('express-handlebars');
-const randomData = require('./faker');
+const Container = require('./container.js');
+const router = require('./routes/router');
+const { optionsMariaDB, optionsSQLite3 } = require('./options/config.js');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 const PORT = 8080;
 const app = express();
 const httpserver = new HttpServer(app);
 const io = new IOServer(httpserver);
 
-app.use(express.static('views'));
+const products = new Container(optionsSQLite3, 'products');
+const messages = new Container(optionsMariaDB, 'messages');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://estebanzarate:nERlfPKfZtCRTu0K@cluster0.hggfd.mongodb.net/?retryWrites=true&w=majority',
+        mongoOptions: advancedOptions,
+        ttl: 60,
+        collectionName: 'sessions'
+    }),
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+app.use(router);
+app.use(express.static('views'));
 app.engine('handlebars', engine());
 app.set('views', './views');
 app.set('view engine', 'handlebars');
 
-app.get('/api/products-test', (req, res) => {
-    res.render('table');
-});
 
 io.on('connection', async socket => {
     console.log('Conexión establecida');
-    const data = randomData();
-    io.sockets.emit('products', data);
-    socket.on('product', async data => {
-        io.sockets.emit('products', data);
+
+    const dbProducts = await products.getAll();
+    io.sockets.emit('products', dbProducts);
+    const dbMessages = await messages.getAll();
+    io.sockets.emit('messages', dbMessages);
+
+    socket.on('product', async product => {
+        products.save(product);
+        const dbProducts = await products.getAll();
+        io.sockets.emit('products', dbProducts);
+    })
+    socket.on('message', async message => {
+        messages.save(message);
+        const dbMessages = await messages.getAll();
+        io.sockets.emit('messages', dbMessages);
     })
 });
 
